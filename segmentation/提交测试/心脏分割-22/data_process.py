bfc854fdcd8faf116ed0a633b945dffbff81aec7
pw s <78252964+polarispw@users.noreply.github.com>
2022-06-03 09:14:57 +0800

    .

---
+++ segmentation/提交测试/心脏分割-22/data_process.py
@@ -0,0 +1,90 @@
+import albumentations as albu
+from my_dataset import Dataset
+import matplotlib.pyplot as plt
+
+def get_training_augmentation():
+    train_transform = [
+
+        albu.HorizontalFlip(p=0.5),
+
+        albu.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=1, border_mode=0),
+
+        albu.Resize(320, 320),
+
+        albu.GaussNoise(p=0.2),
+        albu.Perspective(p=0.5),
+
+        albu.OneOf(
+            [
+                albu.CLAHE(p=1),
+                albu.RandomBrightnessContrast(p=1),
+                albu.RandomGamma(p=1),
+            ],
+            p=0.9,
+        ),
+
+        albu.OneOf(
+            [
+                albu.Sharpen(p=1),
+                albu.Blur(blur_limit=3, p=1),
+                albu.MotionBlur(blur_limit=3, p=1),
+            ],
+            p=0.9,
+        ),
+
+        albu.OneOf(
+            [
+                albu.RandomBrightnessContrast(p=1),
+                albu.HueSaturationValue(p=1),
+            ],
+            p=0.9,
+        ),
+    ]
+    return albu.Compose(train_transform)
+
+
+def get_validation_augmentation():
+    """Add paddings to make image shape divisible by 32"""
+    test_transform = [albu.Resize(384, 480)]
+    return albu.Compose(test_transform)
+
+
+def to_tensor(x, **kwargs):
+    return x.transpose(2, 0, 1).astype('float32')
+
+
+def get_preprocessing(preprocessing_fn):
+    _transform = [
+        albu.Lambda(image=preprocessing_fn),
+        albu.Lambda(image=to_tensor, mask=to_tensor),
+    ]
+    return albu.Compose(_transform)
+
+if __name__=='__main__':
+
+    x_train_dir = ["Heart Data/Image_DCM/png/Image/01/image1.png"]
+    y_train_dir = ["Heart Data/Image_DCM/png/Label/01/label1.png"]
+
+    augmented_dataset = Dataset(
+        x_train_dir,
+        y_train_dir,
+        augmentation=get_training_augmentation(),
+    )
+
+    def visualize(**images):
+        n = len(images)
+        plt.figure(figsize=(16, 5))
+        for i, (name, image) in enumerate(images.items()):
+            plt.subplot(1, n, i + 1)
+            plt.xticks([])
+            plt.yticks([])
+            plt.title(' '.join(name.split('_')).title())
+            plt.imshow(image, cmap='gray')
+        plt.show()
+
+    image, mask = augmented_dataset[0]
+    visualize(
+        image=image,
+        mask1=mask[:, :, 0],
+        mask2=mask[:, :, 1],
+        mask3=mask[:, :, 2], )
